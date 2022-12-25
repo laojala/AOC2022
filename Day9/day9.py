affc675d9f30f9004a5103f60322a21083406fb6
Laura Ojala <laojala@gmaile.com>
2022-12-25 11:33:50 +0200

    refactoring day9

--- Day9/day9.py
+++ Day9/day9.py
@@ -4,17 +4,23 @@ import os
 HEAD_MASK = {'R':(1,0), 'L':(-1,0), 'D':(0,-1), 'U':(0,1)}
 DIFF_MASK = {2:-1, -2:1}
 
+
 class Rope:
-    def __init__(self, length=1):
+    def __init__(self, motions, length=1):
+        self.motions = motions
         self.length = length
         self.head = (0,0)
-        self.knots = self.create_knots(self.length)
+        self.knots = self._create_knots(self.length)
         self.tail_visited = {(0,0)}  # set
 
-    def create_knots(self, length):
+    def run_instructions(self):
+        for motion in self.motions:
+            self._move(motion)
+
+    def _create_knots(self, length):
         return dict.fromkeys(range(length), (0,0))
 
-    def move(self, instruction):
+    def _move(self, instruction):
         # pylint: disable=consider-using-dict-items
         for index in range(int(instruction[1])):
             # new position for head:
@@ -36,7 +42,7 @@ class Rope:
                 if abs(diff[1]) > 1:
                     mask1 = DIFF_MASK[diff[1]]
 
-                if not (abs(diff[0]) <= 1 and abs(diff[1]) <= 1):
+                if mask0 != 0 or mask1 != 0:
                     self.knots[key] = tuple(map(sum,zip(previous, (mask0, mask1))))
                 else:
                     # rest of the nots do not move if previous knot does not move
@@ -54,19 +60,17 @@ def read_input():
 
 def solve() -> None:
     motions = read_input()
-    rope = Rope()
-    for motion in motions:
-        rope.move(motion)
+    rope = Rope(motions)
+    rope.run_instructions()
     part1 = len(rope.tail_visited)
     print(f'part1: {part1}')
-    assert part1 in (13, 5710)
+    assert part1 in (13, 88, 5710)
 
-    rope_part2 = Rope(9)
-    for motion in motions:
-        rope_part2.move(motion)
+    rope_part2 = Rope(motions, 9)
+    rope_part2.run_instructions()
     part2 = len(rope_part2.tail_visited)
     print(f'part2: {part2}')
-    assert part2 in (1, 2259)
+    assert part2 in (1, 36, 2259)
 
 if __name__ == '__main__':
     solve()
